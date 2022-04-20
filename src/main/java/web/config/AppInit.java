d31ccc289c9d1d3bea0c2953e6e44e5f0c31a21e
kosiseno <kindkosiseno@yandex.ru>
2022-04-21 01:57:49 +0300

    Error creating bean with name 'entityManagerFactory'

---
+++ src/main/java/web/config/AppInit.java
@@ -0,0 +1,37 @@
+package web.config;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    // Метод, указывающий на класс конфигурации
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                PersistenceJPAConfig.class
+        };
+    }
+
+
+    // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp.
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+
+    /* Данный метод указывает url, на котором будет базироваться приложение */
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+
+}
